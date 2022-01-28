6f3b12a9c4fdd281b3258534865971b12058fa20
kagemiku <kagemiku@gmail.com>
2022-01-29 08:19:39 +0900

    init project

---
+++ xcode-cloud-test-app/xcode-cloud-test-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  xcode-cloud-test-app
+//
+//  Created by Akira Fukunaga on 2022/01/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ xcode-cloud-test-app/xcode-cloud-test-app/xcode_cloud_test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcode_cloud_test_appApp.swift
+//  xcode-cloud-test-app
+//
+//  Created by Akira Fukunaga on 2022/01/29.
+//
+
+import SwiftUI
+
+@main
+struct xcode_cloud_test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
